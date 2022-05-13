aa956773bbbe4a6e8c90ae63958b7df08f4f328e
ashu kumar <ashu90114@gmail.com>
2022-05-13 22:04:45 +0530

    tried to add google auth but can't

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 
+const passport = require("passport");
+const cookieSession = require('cookie-session')
+require('./public/passport-setup');
 
+app.use(cookieSession({
+  name: 'tuto-session',
+  keys: ['key1', 'key2']
+}))
 
 
 app.set("view engine", "ejs");
@@ -12,10 +21,27 @@ const io = require("socket.io")(server, {
     origin: '*'
   }
 });
+
+
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
+
+
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+// Initializes passport and passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use("/peerjs", peerServer);
 app.use(express.static("public"));
@@ -24,13 +50,30 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-
-
-
-
 app.get("/index", (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
+
+
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureFlash: true, failureRedirect: '/login', successRedirect: '/resume' }));
+
+
+// app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+//   function (req, res) {
+
+//     res.redirect('/index');
+//   }
+// );
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+})
+
 
 
 app.get("/:room", (req, res) => {
@@ -47,4 +90,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+
+
+
+
+server.listen(process.env.PORT || 3000);
